712c2c046c9c3530948facd828c0c606dc7651a2
Yehor Holovan <egorgolovan89@gmail.com>
2025-02-10 19:22:06 +0100

    Created separate component for each project's section for better reusability additionally splitted content for Tabs and Tab Button into separate components

---
+++ src/components/Tabs.jsx
@@ -0,0 +1,13 @@
+export default function Tabs({ children, buttons }) {
+    return (
+        <>
+        <menu>
+            {buttons}
+        </menu>
+
+        {children}
+
+    </>
+    );
+    
+}
